260328805e897d3b7bebd5bd09385c25ab4b6b0d
Ilson Nóbrega <hello@ilson.dev>
2022-10-16 16:35:16 -0300

    adding some libraries, configuring tests and adding some examples

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { RequestContextModule } from 'nestjs-request-context';
+import { postgresConnectionUri } from './configs/database.config';
+import { CqrsModule } from '@nestjs/cqrs';
+import { SlonikModule } from 'nestjs-slonik';
+import { ContextInterceptor } from '@libs/application/context/ContextInterceptor';
+import { ExceptionInterceptor } from '@libs/application/interceptors/exception.interceptor';
+
+const interceptors = [
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: ContextInterceptor,
+  },
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: ExceptionInterceptor,
+  },
+];
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    EventEmitterModule.forRoot(),
+    RequestContextModule,
+    SlonikModule.forRoot({
+      connectionUri: postgresConnectionUri,
+    }),
+    CqrsModule,
+  ],
+  controllers: [],
+  providers: [...interceptors],
 })
 export class AppModule {}
